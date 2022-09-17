f7dbd568cfb601b998ef86b5047abbdf6fd6f666
Tabitha Wong <tabi159w@gmail.com>
2022-09-17 13:48:37 -0400

    um

--- backend/.venv/bin/activate.fish
+++ backend/.venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/tabithawong/Documents/HTN2022/.venv"
+set -gx VIRTUAL_ENV "/Users/tabithawong/Documents/HTN2022/HTN2022/backend/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
