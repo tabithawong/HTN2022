6f4b04e44521900050ccc9899719baa266f0eac8
Tabitha Wong <tabi159w@gmail.com>
2022-09-17 09:48:46 -0400

    ok

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import flask
+import json
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def start():
+    return "Hello world!"
+
+@app.route('/users', methods=["GET"])
+def users():
+    print("users endpoint reached...")
+    with open("users.json", "r") as f:
+        data = json.load(f)
+        data.append({"username": "user4", "pets": ["hamster"]})
+        return flask.jsonify(data)
+
+
