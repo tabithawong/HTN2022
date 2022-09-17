e0f5d3d5745dc7b4e1af6975b3ee4a10269e6257
Tabitha Wong <tabi159w@gmail.com>
2022-09-17 17:53:20 -0400

    Update emotivate.py

--- backend/emotivate.py
+++ backend/emotivate.py
@@ -18,6 +18,9 @@ pd.set_option('display.max_colwidth', None)
 import random
 import math
 
+api_key = 'Pl9CRr5m9EgVoN0zx8QZDVr7G9pXqQGk2weUGKRz'
+co = cohere.Client(api_key)
+
 app = Flask(__name__)
 CORS(app)
 
@@ -42,101 +45,99 @@ def users():
         message = received_data['data']
         arr.append(message)
         print(arr)
+        print(afunc(arr))
         return_data = {"status": "success", "message": f"received: {message}"}
         return flask.Response(response=json.dumps(return_data), status=201)
 
-api_key = 'Pl9CRr5m9EgVoN0zx8QZDVr7G9pXqQGk2weUGKRz'
-co = cohere.Client(api_key)
+def afunc(arr):
+    d = {'text': ["I had a wonderful day!", "I had good day", "My day was alright", "My day was bad", "My day was terrible"]}
+    user_input = {'text': arr}
 
-d = {'text': ["I had a wonderful day!", "I had good day", "My day was alright", "My day was bad", "My day was terrible"]}
+    d = pd.DataFrame(data=d)
+    user_input = pd.DataFrame(user_input)
 
-# you need to give me arr here
+    embeds = co.embed(texts=list(d["text"]), model="large", truncate="LEFT").embeddings
+    embeds = np.array(embeds)
 
-user_input = {'text': arr}
+    search_index = AnnoyIndex(embeds.shape[1], 'angular')
+    for i in range(len(embeds)):
+        search_index.add_item(i, embeds[i])
+    search_index.build(3)
+    search_index.save('test.ann')
 
-d = pd.DataFrame(data=d)
-user_input = pd.DataFrame(user_input)
+    def getConnections(user_input):
+        arr = []
+        for i in range(user_input.shape[0]):
+            similar_item_ids = search_index.get_nns_by_item(i,3, include_distances=True)
+            arr.append([i, similar_item_ids[0][1:]])
+        return arr
 
-embeds = co.embed(texts=list(d["text"]), model="large", truncate="LEFT").embeddings
-embeds = np.array(embeds)
+    connections = getConnections(user_input)
+    #connections is the second thing we need to give anton
 
-search_index = AnnoyIndex(embeds.shape[1], 'angular')
-for i in range(len(embeds)):
-    search_index.add_item(i, embeds[i])
-search_index.build(3)
-search_index.save('test.ann')
-
-def getConnections(user_input):
-    arr = []
-    for i in range(user_input.shape[0]):
-        similar_item_ids = search_index.get_nns_by_item(i,3, include_distances=True)
-        arr.append([i, similar_item_ids[0][1:]])
-    return arr
-
-connections = getConnections(user_input)
-#connections is the second thing we need to give anton
-
-def getSpots(user_input, d):
-    arr = []
-    print(user_input["text"])
-    count = 0
-    for i in user_input["text"]:
-        query = i
-        #print("HELP ME")
-        #print(i)
-        query_embed = co.embed(texts=[query],
-                  model="large",
-                  truncate="LEFT").embeddings
-        similar_item_ids = search_index.get_nns_by_vector(query_embed[0],10, include_distances=True)
-        arr.append([count, query, d.iloc[similar_item_ids[0][0]]['text'], random.uniform(0, 1)])
-        count += 1
-    print(arr)
-    for i in arr:
+    def getSpots(user_input, d):
+        arr = []
+        print(user_input["text"])
         count = 0
-        for j in d["text"]:
-            #print(i[2], j)
-            if i[2] == j:
-                print(count/5)
-                i[2] = (0.8 - count/5) + 0.1 + random.uniform(-0.05, 0.05)
-                break
+        for i in user_input["text"]:
+            query = i
+            #print("HELP ME")
+            #print(i)
+            query_embed = co.embed(texts=[query],
+                    model="large",
+                    truncate="LEFT").embeddings
+            similar_item_ids = search_index.get_nns_by_vector(query_embed[0],10, include_distances=True)
+            arr.append([count, query, d.iloc[similar_item_ids[0][0]]['text'], random.uniform(0, 1)])
             count += 1
-    return arr
+        print(arr)
+        for i in arr:
+            count = 0
+            for j in d["text"]:
+                #print(i[2], j)
+                if i[2] == j:
+                    print(count/5)
+                    i[2] = (0.8 - count/5) + 0.1 + random.uniform(-0.05, 0.05)
+                    break
+                count += 1
+        return arr
 
-nodes = getSpots(user_input, d)
-# first thing we need to give ainthinee
+    nodes = getSpots(user_input, d)
+    # first thing we need to give ainthinee
 
-def computeStats(main_arr):
-    mean = 0
-    l = len(main_arr)
-    for i in main_arr:
-        mean += i[2]
-    mean /= l
-    sd = 0
-    for i in main_arr:
-        sd += (i[2] - mean)
-    sd /= l
-    sd = math.sqrt(sd)
-    if (sd > 1):
-        st = "There is high standard deviations in employees today"
-    if (sd > 2):
-        st = "There is extremely high standard deviations in employees today"
-    else:
-        st = "Most employees are feeling the same today"
-    if (mean > 0.8):
-        m = "On average the employees are feeling really happy today"
-    elif (mean > 0.6):
-        m = "On average the employees are feeling happiesh today"
-    elif (mean < 0.2):
-        m = "On average the employees are feeling very low today"
-    elif (mean < 0.4):
-        m = "On average the employees are feeling sadish today"
-    else:
-        m = "They're alive"
-    return m, st
+    def computeStats(main_arr):
+        mean = 0
+        l = len(main_arr)
+        for i in main_arr:
+            mean += i[2]
+        mean /= l
+        sd = 0
+        for i in main_arr:
+            sd += (i[2] - mean)
+        sd /= l
+        sd = math.sqrt(sd)
+        if (sd > 1):
+            st = "There is high standard deviations in employees today"
+        if (sd > 2):
+            st = "There is extremely high standard deviations in employees today"
+        else:
+            st = "Most employees are feeling the same today"
+        if (mean > 0.8):
+            m = "On average the employees are feeling really happy today"
+        elif (mean > 0.6):
+            m = "On average the employees are feeling happiesh today"
+        elif (mean < 0.2):
+            m = "On average the employees are feeling very low today"
+        elif (mean < 0.4):
+            m = "On average the employees are feeling sadish today"
+        else:
+            m = "They're alive"
+        return m, st
 
-ms, sds = computeStats(nodes)
+    ms, sds = computeStats(nodes)
+
+    return nodes, connections, ms, sds
+
 # third thing to pass to mantoine
 
 if __name__ == "__main__":
     app.run("localhost", 6969)
-
