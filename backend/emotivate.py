6e7af2e9da321258d8f9a432c3ee11fa73895f38
Tabitha Wong <tabi159w@gmail.com>
2022-09-17 17:13:38 -0400

    combining python files

--- backend/emotivate.py
+++ backend/emotivate.py
@@ -1,3 +1,7 @@
+from flask import Flask, request
+import flask
+import json
+from flask_cors import CORS
 import cohere
 import numpy as np
 import re
@@ -14,6 +18,33 @@ pd.set_option('display.max_colwidth', None)
 import random
 import math
 
+app = Flask(__name__)
+CORS(app)
+
+arr = []
+
+@app.route('/users', methods=["GET", "POST"])
+def users():
+    print("users endpoint reached")
+    if request.method == "GET":
+        print("get request")
+        with open("users.json", "r") as f:
+            data = json.load(f)
+            data.append({
+                "username": "user4",
+                "pets": ["hamster"]
+            })
+            return flask.jsonify(data)
+    if request.method == "POST":
+        print("post request")
+        received_data = request.get_json()
+        print(f"received data: {received_data}")
+        message = received_data['data']
+        arr.append(message)
+        print(arr)
+        return_data = {"status": "success", "message": f"received: {message}"}
+        return flask.Response(response=json.dumps(return_data), status=201)
+
 api_key = 'Pl9CRr5m9EgVoN0zx8QZDVr7G9pXqQGk2weUGKRz'
 co = cohere.Client(api_key)
 
@@ -21,7 +52,7 @@ d = {'text': ["I had a wonderful day!", "I had good day", "My day was alright", 
 
 # you need to give me arr here
 
-user_input = {'text': []}
+user_input = {'text': arr}
 
 d = pd.DataFrame(data=d)
 user_input = pd.DataFrame(user_input)
@@ -105,3 +136,7 @@ def computeStats(main_arr):
 
 ms, sds = computeStats(nodes)
 # third thing to pass to mantoine
+
+if __name__ == "__main__":
+    app.run("localhost", 6969)
+
